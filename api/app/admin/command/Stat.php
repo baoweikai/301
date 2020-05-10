414e85453a594622901b1615e357e80793c371ae
wk <weikai99999@gmail.com>
2020-05-10 20:30:25 +0800

    111111111

--- api/app/admin/command/Stat.php
+++ api/app/admin/command/Stat.php
@@ -6,12 +6,16 @@ use think\console\Input;
 use think\console\input\Argument;
 use think\console\input\Option;
 use think\console\Output;
+use think\facade\Log;
+use think\facade\Db;
+use helper\Http;
+use app\admin\model\Stat as Model;
 
 class Stat extends Command
 {
     protected function configure()
     {
-        $this->setName('fresh')
+        $this->setName('stat')
         	->addArgument('name', Argument::OPTIONAL, "your name")
             ->addOption('city', null, Option::VALUE_REQUIRED, 'city name')
         	->setDescription('统计数据');
@@ -20,6 +24,7 @@ class Stat extends Command
     // 调用update 这个类时,会自动运行execute方法
     protected function execute(Input $input, Output $output)
     {
+
         $output->writeln('执行完成');
     }
 }
