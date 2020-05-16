96478718cebce25d5787710168fdb0ab236f1dc0
wk <weikai99999@gmail.com>
2020-05-16 18:03:08 +0800

    11111

--- cdn/app/controller/Error.php
+++ cdn/app/controller/Error.php
@@ -7,14 +7,14 @@ use think\facade\Db;
 use app\model\CitedDomain;
 use app\model\Group;
 
-class Index extends BaseController
+class Error extends BaseController
 {
-    protected  $redis, $date, $jump_url = '', $ip = '', $did = 0, $default = 'https://www.95egg.com';
     public function __call($method, $args)
     {
         return $this->index();
     }
-    protected function initialize(){      
+    protected  $redis, $date, $jump_url = '', $ip = '', $did = 0, $default = 'https://www.95egg.com';
+    protected function initialize(){
         // ip处理
         $this->ip = get_ip();
         $ipLast = substr($this->ip, -1);
@@ -22,7 +22,7 @@ class Index extends BaseController
         $i = $ipLast % count(config('cache.stores'));
         // $redis配置
         $config = config('cache.stores.redis' . $i);
-        $this->redis = (new Redis($config))->handler();
+        $this->redis = new Redis($config);
     }
 
     public function index()
@@ -39,17 +39,17 @@ class Index extends BaseController
         $host = request()->host();
         // 查询数据
         $domain = $this->redis->hget('DomainList', $host);
+
         if($domain === false){
             $this->ip();
             $this->cited();
-            return redirect($this->jump_url, 301);
-        } else{
-            $domain = unserialize($domain);
+            return redirect($this->jump_url);
         }
+        $domain = unserialize($domain);
 
         //验证后缀是否是图片，js，css等
         if(verifyExt(request()->ext())){
-            return redirect($domain["jump_host"] . request()->url(), 301);
+            return redirect($domain["jump_host"] . request()->url());
         }
 
         $this->did = $domain['id'];
@@ -71,7 +71,7 @@ class Index extends BaseController
             $this->jump();
         }
 
-        return redirect($this->jump_url, 301);
+        return redirect($this->jump_url);
     }
 
     /**
@@ -79,7 +79,7 @@ class Index extends BaseController
      */
     private function ip()
     {
-        // 如果ip 不存在当日ip列表中，则增加ip统计
+        // 如果ip 不存在当日ip，则增加ip统计
         if(!$this->redis->sismember('IpList' . $this->date, $this->did . '_' . $this->ip)){
             $this->redis->hincrby('IpCount' . $this->date, $this->did, 1);
             $this->redis->sadd('IpList' . $this->date,  $this->did . '_' . $this->ip);
@@ -99,26 +99,24 @@ class Index extends BaseController
     //引量统计
     private function cited($groupId = null){
         $citeds = $this->redis->get('Citeds');
-        if($citeds === false){
+        if($citeds === null){
             $citeds = CitedDomain::cache();
-            $this->redis->set('Citeds', serialize($citeds));
-        } else {
-            $citeds = unserialize($citeds);
+            $this->redis->set('Citeds', $citeds);
         }
         if($groupId === null){
             $groupId = $this->redis->get('DefaultGroupId');
-            if($groupId === false){
+            if($groupId === null){
                 $groupId = Group::where('status', '=', 1)->order(['is_default' => 'desc', 'id' => 'ASC'])->value('id');
                 $this->redis->set('DefaultGroupId', $groupId);
-            }
+            }    
         }
-        $domains = isset($citeds[$groupId]) ? $citeds[$groupId] : [];
+        $domains = $groupId !== null && isset($citeds[$groupId]) ? $citeds[$groupId] : [];
         $l = count($domains);
         $this->jump_url = $l > 0 ? $domains[mt_rand(0, $l - 1)] : $this->default;
 		$this->jump_url .= '?' . $this->did;
 		
         $this->redis->hincrby('CitedCount' . $this->date, $this->did, 1);
-        // 引量ip列表
+        // 引流ip列表
         $this->redis->hset('CitedIpList', $this->did . $this->ip, time());
 
         $fh = fopen(runtime_path() . '/' . $this->date, "a");
